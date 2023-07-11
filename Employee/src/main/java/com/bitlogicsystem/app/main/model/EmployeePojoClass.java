1108b277835e89b41cf078b7498d23fe7a81c77e
JayaGunjkar <jayagunjkar4428@gmail.com>
2023-07-11 15:27:33 +0530

    initialEmp

---
+++ Employee/src/main/java/com/bitlogicsystem/app/main/model/EmployeePojoClass.java
@@ -0,0 +1,20 @@
+package com.bitlogicsystem.app.main.model;
+
+public class EmployeePojoClass {
+
+	private int id;
+	private String name;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+}
